7129d1200ac7418902845bf76b42a383473dac42
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-12-16 19:46:46 +0530

    started

---
+++ server/index.js
@@ -0,0 +1,28 @@
+
+const express  = require("express")
+// const connection  = require("./database/db")
+// const blogRouter = require("./routes/blog")
+// const productRouter = require("./routes/product")
+// const authRouter = require("./routes/Auth")
+const cors = require("cors")
+
+const app = express()
+app.use(express.json())
+// app.use('/uploads',express.static('uploads'))
+app.use(express.urlencoded({extended:true}))
+// app.use("/blog",blogRouter)
+// app.use("/product",productRouter)
+// app.use("/auth",authRouter)
+
+app.use(cors({
+    origin:"*"
+  }))
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Home Page")
+})
+
+app.listen(8080, async()=>{
+    // await connection
+    console.log("server started at http://localhost:8080");
+})
